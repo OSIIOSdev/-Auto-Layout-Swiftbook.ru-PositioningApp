6e0c6b3ca2890b0c98549f10607a05f27818d9cf
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-06 14:31:51 -0800

    1. The elements are placed on the screen

--- PositioningApp/ViewController.swift
+++ PositioningApp/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    var blueView = UIView()
+    var redView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        
+        setupLayout()
     }
-
-
+    
+    private func setupLayout() {
+        configureBlueView()
+        configureRedView()
+    }
+    
+    private func configureBlueView() {
+        view.addSubview(blueView)
+        
+        blueView.backgroundColor = .systemBlue
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            blueView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            blueView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+            blueView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40)])
+    }
+    
+    private func configureRedView() {
+        view.addSubview(redView)
+        
+        redView.backgroundColor = .systemRed
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            redView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            redView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+            redView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            redView.topAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 40),
+            redView.heightAnchor.constraint(equalTo: blueView.heightAnchor, multiplier: 1)])
+    }
 }
 
